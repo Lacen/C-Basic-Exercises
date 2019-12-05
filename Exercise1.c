2555c161facbf01b907c278539bbd98efd128aa4
Lacen <43798572+Lacen@users.noreply.github.com>
2019-12-05 12:15:22 -0700

    Add files via upload
    Exercise 1

---
+++ Exercise1.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+
+
+int main() {
+
+
+	//print out the Name, Birthday and Mobile Phone
+	printf("NAME: John Doe\n");
+	printf("DOB: March 21, 1992\n");
+	printf("Mobile: 24-22221");
+	return 0;
+
+
+
+
+}
